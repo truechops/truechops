4ce1b2c7213c88220083ef65568dc9be3bcf57ce
Jared Simon <jared.d.simon@gmail.com>
2021-07-09 12:40:09 -0500

    Delete part confirmation.

--- src/components/compose/Sidebar.js
+++ src/components/compose/Sidebar.js
@@ -12,6 +12,15 @@ import _ from "lodash";
 import { FaTrash, FaPlus } from "react-icons/fa";
 import { useTheme } from "@material-ui/core/styles";
 
+import React from "react";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
@@ -20,10 +29,10 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
   },
   trash: {
-      marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   select: {
-    textAlign: 'left'
+    textAlign: "left",
   },
   subheader: {
     fontSize: 20,
@@ -33,26 +42,38 @@ const useStyles = makeStyles((theme) => ({
 export default function Sidebar() {
   const classes = useStyles();
   const unusedInstrumentsRef = useRef();
+  const [modalOpen, setModalOpen] = useState(false);
   const theme = useTheme();
   const partConfig = useSelector((state) => state.score.present.score.parts);
-  const unusedInstruments = useSelector(state => Object.keys(state.score.present.voices)
-                           .filter(voice => !Object.keys(partConfig).includes(voice)));
+  const unusedInstruments = useSelector((state) =>
+    Object.keys(state.score.present.voices).filter(
+      (voice) => !Object.keys(partConfig).includes(voice)
+    )
+  );
 
+  const [instrumentToDelete, setInstrumentToDelete] = useState('');
   const [selectedInstrument, setSelectedInstrument] = useState('');
 
   function onChangeInstrument(event) {
-      setSelectedInstrument(event.target.value);
+    setSelectedInstrument(event.target.value);
+  }
+
+  function deletePart(instrument) {
+    dispatch(scoreActions.deletePart(instrument));
+    setModalOpen(false);
   }
 
   useEffect(() => {
-      if(unusedInstruments.length > 0 && (!unusedInstrumentsRef.current || !_.isEqual(unusedInstrumentsRef.current, unusedInstruments))) {
-          console.log('a: ' + unusedInstruments[0]);
-        setSelectedInstrument(unusedInstruments[0]);
+    if (
+      unusedInstruments.length > 0 &&
+      (!unusedInstrumentsRef.current ||
+        !_.isEqual(unusedInstrumentsRef.current, unusedInstruments))
+    ) {
+      setSelectedInstrument(unusedInstruments[0]);
 
-        unusedInstrumentsRef.current = unusedInstruments;
-      }
-
-  }, [unusedInstruments])
+      unusedInstrumentsRef.current = unusedInstruments;
+    }
+  }, [unusedInstruments]);
 
   const dispatch = useDispatch();
   var instrumentConfig = [];
@@ -63,7 +84,7 @@ export default function Sidebar() {
       label: part,
       checked: partConfig[clonedPart].enabled,
       onChange: () => dispatch(scoreActions.togglePartEnabled(clonedPart)),
-      onDelete: () => dispatch(scoreActions.deletePart(clonedPart))
+      onDelete: () => { setInstrumentToDelete(clonedPart); setModalOpen(true) },
     });
   }
 
@@ -92,24 +113,56 @@ export default function Sidebar() {
   ));
 
   return (
-    <List
-      subheader={
-        <ListSubheader classes={{ root: classes.subheader }}>
-          Parts
-        </ListSubheader>
-      }
-      className={classes.root}
-    >
-        {unusedInstruments.length > 0 && <><Select
-        className={classes.select}
-          value={selectedInstrument}
-          native
-          onChange={onChangeInstrument}
-        >
-            {unusedInstruments.map(instrument => <option key={Math.random().toString()} value={instrument}>{instrument}</option>)}
-        </Select>
-        <Button onClick={() => dispatch(scoreActions.addPart(selectedInstrument))}><FaPlus /></Button></>}
-      {listItems}
-    </List>
-  );
+    <>
+      <List
+        subheader={
+          <ListSubheader classes={{ root: classes.subheader }}>
+            Parts
+          </ListSubheader>
         }
+        className={classes.root}
+      >
+        {unusedInstruments.length > 0 && (
+          <>
+            <Select
+              className={classes.select}
+              value={selectedInstrument}
+              native
+              onChange={onChangeInstrument}
+            >
+              {unusedInstruments.map((instrument) => (
+                <option key={Math.random().toString()} value={instrument}>
+                  {instrument}
+                </option>
+              ))}
+            </Select>
+            <Button
+              onClick={() => dispatch(scoreActions.addPart(selectedInstrument))}
+            >
+              <FaPlus />
+            </Button>
+          </>
+        )}
+        {listItems}
+      </List>
+
+      <Dialog
+        maxWidth="xs"
+        aria-labelledby="confirmation-dialog-title"
+        open={modalOpen}
+      >
+        <DialogContent dividers>
+          Are you sure you want to delete the &apos;{instrumentToDelete}&apos; part?
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={setModalOpen.bind(null, false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={deletePart.bind(null, instrumentToDelete)} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
