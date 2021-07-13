ef9ece836f5f668a5ed4adffcc4533e5f614da48
Jared Simon <jared.d.simon@gmail.com>
2021-07-13 12:52:42 -0500

    save rhythm modal to ask for rhythm name. cleanup.

--- pages/library.js
+++ pages/library.js
@@ -1,10 +1,9 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-const DynamicMain = dynamic(
-    () => import('../src/components/library/Main'),
-    { ssr: false }
-  )
+const DynamicMain = dynamic(() => import("../src/components/library/Main"), {
+  ssr: false,
+});
 
 export default function mine() {
-    return <DynamicMain />
+  return <DynamicMain />;
 }
