143519ec3da6954fd098c4128f44e1fbf0b6d9b9
Jared Simon <jared.d.simon@gmail.com>
2021-07-31 14:08:21 -0500

    dont use Safari dialog.

--- pages/about.js
+++ pages/about.js
@@ -9,17 +9,6 @@ import Copyright from '../src/Copyright';
 
 export default function About() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    TrueChops is wri
   );
 }
