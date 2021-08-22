f298956cb2ff03c809c23b2e1f14fd9a4de20926
giannis <ichionid@gmail.com>
2021-08-22 15:01:20 +0200

    Check that RA calculates the new values correctly. Next step is to create the new rhythm. So decode the data again. Linting, and then start spreading generic functions to services

--- src/store/services/mutate/types/raEmergentCycles.js
+++ src/store/services/mutate/types/raEmergentCycles.js
@@ -10,26 +10,20 @@ export default function raEmergentCycles({ probability }, notes) {
     if(currentNote != null) {
       for (let j = 0, lengthJ = currentNote.notes.length; j < lengthJ; j++) {
         let drumsoundOnNote = notes[i].notes[j];
-        if (drumSounds.hasOwnProperty(drumsoundOnNote) === false) {
-          drumSounds.push({note: drumsoundOnNote, data: new Array(notes.length).fill(0)});
+        let noteIndex = findWithAttr(drumSounds, "note", drumsoundOnNote);
+
+        //if (drumSounds.hasOwnProperty(drumsoundOnNote) === false) {
+        if( noteIndex == -1) {
+          let initialRhythmicScheme = new Array(notes.length).fill(0);
+          initialRhythmicScheme[i]=1;
+          drumSounds.push({note: drumsoundOnNote, data: initialRhythmicScheme});
+        } else {
+          drumSounds[noteIndex].data[i]=1;
         }
-        // Set the actual note.
-        console.log(findWithAttr(drumSounds, "note", drumsoundOnNote));
-        //drumSounds[][i] = 1;
       }
     }
   }
-  console.log(drumSounds);
-  //emergentCycles(notes, drumSounds);
-}
-
-function findWithAttr(array, attr, value) {
-    for(var i = 0; i < array.length; i += 1) {
-        if(array[i][attr] === value) {
-            return i;
-        }
-    }
-    return -1;
+  emergentCycles(notes, drumSounds);
 }
 
 /**
@@ -41,31 +35,50 @@ function findWithAttr(array, attr, value) {
  *   It contains an array of drum sounds of the same length.
  **/
 function emergentCycles(notes, drumSounds) {
-  console.log("emergentCycles");
-    console.log(drumSounds);
-
-  for (let [index, val] of drumSounds.entries()) {
-    console.log(11111);
-  }
-
   for (let [i, val] of drumSounds.entries()) {
-    for (let j = 0, lengthJ = drumSounds[i].length; j < lengthJ; j++) {
-      let neighborhoodSize = calculateNeighborhood(drumSounds[i], j);
-      console.log(neighborhoodSize);
+    console.log(val);
+    for (let j = 0, lengthJ = drumSounds[i].data.length; j < lengthJ; j++) {
+      let neighborhoodSize = calculateNeighborhood(drumSounds[i].data, j);
+      drumSounds[i].data[j] = applyEmergentCykles(neighborhoodSize, drumSounds[i].data[j])
     }
   }
-  /*for (let i = 0, lengthI = drumSounds.length; i < lengthI; i++) {
-  }*/
 }
 
 function calculateNeighborhood(drumSoundPattern, index) {
-  if (j>0 && j < drumSoundPattern.length-1) {
+  if (index ==0 ) {
     return drumSoundPattern[index] + drumSoundPattern[index+1]
   }
-  else if (j <= drumSoundPattern.length-1) {
+  else if (index == drumSoundPattern.length-1) {
     return drumSoundPattern[index] + drumSoundPattern[index-1]
   }
   else {
     return drumSoundPattern[index] + drumSoundPattern[index-1] + drumSoundPattern[index+1]
   }
+}
+ // (0) Note, (1) Rest , (2) Rest ,(3) Unchanged.
+ // Make this abstract so that you can pass the type and it'll figure out the rules.
+function applyEmergentCykles(neighborhoodSize, noteValue) {
+  if (neighborhoodSize == 0) {
+    return 1;
+  } else if (neighborhoodSize == 1) {
+    return 0;
+  } else if (neighborhoodSize == 2) {
+    return 0;
+  } else {
+    return noteValue;
+  }
+
+
+}
+
+/**
+ * Function returning the index of an object with a specific param value within an array.
+ */
+function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
 }
